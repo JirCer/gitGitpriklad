44138e5c9a678ace50d955465d5e6b8e8cf7d8b7
JiriCerny <jiricerny7@gmail.com>
2024-12-17 16:31:31 +0100

    Initial commi my first commit

---
+++ src/GitGithub.java
@@ -0,0 +1,20 @@
+public class GitGithub {
+
+    int pocetGit;
+    double priemerGit;
+
+    public GitGithub(int pocetGit, double priemerGit) {
+        this.pocetGit = pocetGit;
+        this.priemerGit = priemerGit;
+    }
+
+    public int getPocetGit() {
+        return pocetGit;
+    }
+
+    public double getPriemerGit() {
+        return priemerGit;
+    }
+
+
+}
